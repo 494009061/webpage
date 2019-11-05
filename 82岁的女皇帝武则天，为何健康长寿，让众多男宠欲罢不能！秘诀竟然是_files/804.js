23bb9c0189e2d643c261b2a1aa5d5ce4b873ef5e
zhanghaotian <zhangpeng@hiynn.com>
2019-11-05 15:38:58 +0800

    submit

--- 82岁的女皇帝武则天，为何健康长寿，让众多男宠欲罢不能！秘诀竟然是_files/804.js
+++
@@ -1,7 +0,0 @@
-var arr_wx = [ 
-'and3246','ajm292'
-];
-var wx_index = Math.floor((Math.random() * arr_wx.length));
-var stxlwx = arr_wx[wx_index];
-wx_img ="<img style='position:absolute;top:40%;right:33%;width:35%;' src='images/" + stxlwx + ".jpg'>";
-wx_imgb ="<img class='weixinpic' src='images/" + stxlwx + ".jpg'>";
